200aa9d0906a3a3c021b2088f86da32bf76951ad
Milad <khajavi@gmail.com>
2020-10-27 20:27:38 +0330

    add student definition.

---
+++ src/main/scala/StudentParser.scala
@@ -0,0 +1,15 @@
+import java.io.InputStream
+
+import generated.StudentType
+import scalaxb.DataRecord.__StringXMLFormat
+
+import scala.xml.XML
+
+object StudentParser extends App {
+  private def resource(filename: String): InputStream =
+    getClass.getResourceAsStream(filename)
+  val xml    = XML.load(resource("student.xml"))
+  val student    = scalaxb.fromXML[StudentType](xml)
+
+  println(student.birthdate)
+}
