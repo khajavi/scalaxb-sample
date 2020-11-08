c17102d23d744d6c1cbf6fce017db94359f6d251
Milad <khajavi@gmail.com>
2020-11-08 20:44:48 +0330

    add toXML snippet for round trip test.

--- src/main/scala/StudentParser.scala
+++ src/main/scala/StudentParser.scala
@@ -5,9 +5,11 @@ import generated.StudentType
 import scala.xml.XML
 
 object StudentParser extends App {
-  private def resource(filename: String): InputStream =
-    getClass.getResourceAsStream(filename)
-  val xml    = XML.load(resource("student.xml"))
-  val student    = scalaxb.fromXML[StudentType](xml)
+  private def resource(file: String): InputStream =
+    getClass.getResourceAsStream(file)
+  val xml = XML.load(resource("student.xml"))
+  val student = scalaxb.fromXML[StudentType](xml)
   println(student.birthdate)
+
+  println(scalaxb.toXML(student, None, "f", generated.defaultScope))
 }
